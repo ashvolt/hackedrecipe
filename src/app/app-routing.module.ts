312b37e2df9979ecb4675dc2c9e6d5f1ba4af68c
ashvolt <pragashvenkat@gmail.com>
2020-08-08 16:43:55 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 
 const routes: Routes = [
   {path: 'recipeList', component: RecipeListComponent},
+   { path: 'recipt-details/:id', component: RecipeDetailsComponent},
   {path: '404',  component: PageNotFoundComponent},
   {path: '', redirectTo:'/recipeList', pathMatch: 'full',},
   {path: '**', redirectTo: '/404'},
